346814b76f544d681843b7d6251dec76f9a35c4d
pepew <yboin@free.fr>
2024-12-12 23:05:43 +0100

    Tap:E, Hold: Ctrl, tapping_term=400

--- keyboards/boardsource/unicorne/keymaps/pepew/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/pepew/keymap.c
@@ -1,3 +1,4 @@
+#include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 #if __has_include("keymap.h")
 #    include "keymap.h"
@@ -208,11 +209,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
              KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_ESC,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-              MO(1),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_DEL,
+              MO(1),    KC_A,    KC_S,    CTL_T(KC_D),   KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_DEL,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
             KC_LGUI,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_END,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_LCTL, LT(3,KC_BSPC), KC_SPC,     KC_ENT, KC_RALT, KC_RSFT
+                                           KC_LALT, LT(3,KC_BSPC), KC_SPC,     KC_ENT, KC_RALT, KC_RSFT
                                             //`--------------------------'  `--------------------------'
         ),
       
