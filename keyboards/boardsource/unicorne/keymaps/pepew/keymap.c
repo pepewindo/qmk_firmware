65a050b781d75d74172695f835b5b63758a75ec7
pepew <yboin@free.fr>
2024-12-29 23:36:33 +0100

    ajout macros pour guillemets avec espaces insécables

--- keyboards/boardsource/unicorne/keymaps/pepew/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/pepew/keymap.c
@@ -197,6 +197,34 @@ static const char PROGMEM ergol_logo_navnum_img[] = {
 };
 
 
+enum custom_keycodes {
+    QMK_GO = SAFE_RANGE,
+    QMK_GC,
+};
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case QMK_GO:
+        if (record->event.pressed) {
+            // when keycode QMK_GO is pressed
+            SEND_STRING( SS_RSFT( SS_TAP(X_2) " " ) );
+        } else {
+            // when keycode QMK_GO is released
+        }
+        break;
+
+    case QMK_GC:
+        if (record->event.pressed) {
+            // when keycode QMK_GC is pressed
+            SEND_STRING( SS_RSFT( " " SS_TAP(X_3) ) );
+        } else {
+            // when keycode QMK_GC is released
+        }
+        break;
+    }
+    return true;
+};
 
 
 /* THIS FILE WAS GENERATED!
@@ -221,7 +249,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
              KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_TRNS, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            KC_TRNS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,   TG(2),
+            KC_TRNS, KC_EXLM,  QMK_GO,  QMK_GC,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,   TG(2),
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
               KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, 
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
