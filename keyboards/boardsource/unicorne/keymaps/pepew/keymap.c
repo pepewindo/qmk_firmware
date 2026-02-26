49c3b46a7c705c6b44c2b4a91af87712468b31b3
pepew <yboin@free.fr>
2026-02-26 22:42:13 +0100

    NavNum change for slash and asterisk

--- keyboards/boardsource/unicorne/keymaps/pepew/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/pepew/keymap.c
@@ -437,14 +437,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NAV_NUM] = LAYOUT_split_3x6_3(
         //|--------+--------+--------+--------+--------+--------|                    |--------+------+------+------+--------+--------|
-             KC_TAB, KC_CAPS, KC_HOME,   KC_UP,  KC_END, KC_PGUP,                      KC_PMNS,  KC_7,  KC_8,  KC_9, KC_PSLS, KC_TRNS,
+             KC_TAB, KC_CAPS, KC_HOME,   KC_UP,  KC_END, KC_PGUP,                      KC_PMNS,  KC_7,  KC_8,  KC_9, KC_PAST, RCTL(KC_BSPC),
         //|--------+--------+--------+--------+--------+--------|                    |--------+------+------+------+--------+--------|
             KC_TRNS,  KC_TAB, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                      KC_PPLS,  KC_4,  KC_5,  KC_6,    KC_0, KC_CALC,
         //|--------+--------+--------+--------+--------+--------|                    |--------+------+------+------+--------+--------|
             KC_TRNS, KC_WBAK, KC_PSCR,  KC_ESC,  KC_INS, KC_WFWD,                         KC_N,  KC_1,  KC_2,  KC_3,  KC_DOT,  KC_DEL,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+------+------+------+--------+--------|
-                                                KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,  KC_DEL,  KC_0
-                                            //`--------------------------'  `------------------------'
+                                                KC_TRNS, KC_TRNS, KC_TRNS,     KC_ENT,  KC_DEL, KC_PSLS
+                                            //`--------------------------'  `--------------------------'
         ),
 
     [_MIR_FUNC] = LAYOUT_split_3x6_3(
