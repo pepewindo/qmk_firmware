fa2b64a65d7d03986ccbcc0b16283306537a4d3e
pepew <yboin@free.fr>
2026-04-26 00:13:46 +0200

    Space on right thumb

---
+++ quantum/keymap_extras/keymap_french_ergol.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "keycodes.h"
+// clang-format off
+
+
+// Aliases
+#define  EL_Q    KC_Q    // Q
+#define  EL_C    KC_W    // C
+#define  EL_O    KC_E    // O
+#define  EL_P    KC_R    // P
+#define  EL_W    KC_T    // W
+#define  EL_J    KC_Y    // J
+#define  EL_M    KC_U    // M
+#define  EL_D    KC_I    // D
+#define  EL_1DK  KC_O    // 1DK
+#define  EL_Y    KC_P    // Y
+#define  EL_A    KC_A    // A
+#define  EL_S    KC_S    // S
+#define  EL_E    KC_D    // E
+#define  EL_N    KC_F    // N
+#define  EL_F    KC_G    // F
+#define  EL_L    KC_H    // L
+#define  EL_R    KC_J    // R
+#define  EL_T    KC_K    // T
+#define  EL_I    KC_L    // I
+#define  EL_U    KC_SCLN // U
+#define  EL_Z    KC_Z    // Z
+#define  EL_X    KC_X    // X
+#define  EL_MINS KC_C    // -
+#define  EL_V    KC_V    // V
+#define  EL_B    KC_B    // B
+#define  EL_DOT  KC_N    // .
+#define  EL_H    KC_M    // H
+#define  EL_G    KC_COMM // G
+#define  EL_COMM KC_DOT  // ,
+#define  EL_K    KC_SLSH // K
+#define  EL_1    KC_1    // 1
+#define  EL_2    KC_2    // 2
+#define  EL_3    KC_3    // 3
+#define  EL_4    KC_4    // 4
+#define  EL_5    KC_5    // 5
+#define  EL_6    KC_6    // 6
+#define  EL_7    KC_7    // 7
+#define  EL_8    KC_8    // 8
+#define  EL_9    KC_9    // 9
+#define  EL_0    KC_0    // 0
